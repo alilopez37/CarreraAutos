d6788ce5e70e78d9d16eba7c4be92143b19e82cc
alilopez <ali.lopez37@gmail.com>
2023-01-16 22:32:43 -0600

    [UPDATE] Uso de recursos

--- src/main/java/com/example/carreraautos/controllers/HelloController.java
+++ src/main/java/com/example/carreraautos/controllers/HelloController.java
@@ -1,14 +1,19 @@
-package com.example.carreraautos;
+package com.example.carreraautos.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane rootScene;
+
+    @FXML
+    public void initialize(){
+        System.out.println(getClass().getResource("/assets/imgs/pista.png"));
+        ImageView fondo = new ImageView(new Image(getClass().getResourceAsStream("/assets/imgs/pista.png")));
+        rootScene.getChildren().add(fondo);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.carreraautos {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
     opens com.example.carreraautos to javafx.fxml;
     exports com.example.carreraautos;
+    exports com.example.carreraautos.controllers;
+    opens com.example.carreraautos.controllers to javafx.fxml;
 }
